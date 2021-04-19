3c1e955912d60a27415ab5c286b2a9ce60fd1b96
MohamedAbdelmagid <mohamed.abdelmagid.1991@gmail.com>
2021-04-19 18:12:40 -0500

    Restructure code

--- server.py
+++ server.py
@@ -5,19 +5,21 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new_db.sqlite3'
 
 db = SQLAlchemy(app)
 
 class Device(db.Model):
+    __tablename__ = 'devices'
+
     id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.String(20))
     ip_address = db.Column(db.String(39))
     port = db.Column(db.String(5))
-    event = db.Column(db.String(20))
+    status = db.Column(db.String(7), default='unknown', nullable=False)
 
     def __repr__(self):
-        return '<Device {}, class as {}>'.format(self.ip_address, self.event)
+        return '<Device {}, class as {}>'.format(self.ip_address, self.status)
     
     def to_dict(self):
         device = {
@@ -25,36 +27,37 @@ class Device(db.Model):
             'type': self.type,
             'ip_address': self.ip_address,
             'port': self.port,
-            'event': self.event,
+            'status': self.status,
         }
         return device
 
 
-@app.route('/<address>', methods=['GET'])
-def authenticate_ip(address):
+@app.route('/<address>/<event>', methods=['GET'])
+def authenticate_ip(address, event):
     current_device_IP = request.remote_addr
     print(current_device_IP)
 
     # Authenticate the current device
     currentDevice = Device.query.filter_by(ip_address=current_device_IP).first()
     if currentDevice:
-        if currentDevice.event == "ubnormal":
+        if currentDevice.status == 'blocked':
             abort(401)
 
     # Find device with this ip in the database
     device = Device.query.filter_by(ip_address=address).first()
     if device:
-        if device.event == "normal":
-            return jsonify({ 'msg': 'The device is normal', 'device': device.to_dict()})
-        else:
-            return jsonify({ 'msg': 'This device is ubnormal', 'device': device.to_dict()})
+        if device.status != 'trusted' and event == 'abnormal':
+            device.status = 'blocked'
+            db.session.commit()
 
+        return jsonify({ 'device': device.to_dict() })
     else:
         # Add new device to database
-        newDevice = Device(ip_address=address, event='ubnormal')
+        newDevice = Device(ip_address=address)
         db.session.add(newDevice)
         db.session.commit()
-        return jsonify({ 'msg': 'Added as ubnormal device', 'device': newDevice.to_dict()})
+
+        return jsonify({ 'device': newDevice.to_dict() })
 
 @app.route('/all', methods=['GET'])
 def get_all_devices():
@@ -66,37 +69,40 @@ def get_all_devices():
 
     return jsonify(data)
 
-@app.route('/add/<address>/<event>', methods=['GET', 'POST'])
-def add_new_device(address, event):
+@app.route('/add/<address>/<status>', methods=['GET', 'POST'])
+def add_new_device(address, status):
     # Find device with this ip in the database
     device = Device.query.filter_by(ip_address=address).first()
     if device:
-        return jsonify({ 'msg': 'Already exist in the database', 'address': address }), 303
+        return jsonify({
+            'msg': 'Already exist in the database',
+            'address': address
+        }), 303
 
     # Add new device to database
-    newDevice = Device(ip_address=address, event=event)
+    newDevice = Device(ip_address=address, status=status)
     db.session.add(newDevice)
     db.session.commit()
 
-    return jsonify({ 'added': newDevice.to_dict()}), 201
+    return jsonify({ 'added': newDevice.to_dict() }), 201
 
-@app.route('/add/me/<event>', methods=['GET', 'POST'])
-def add_me(event):
+@app.route('/add/me', methods=['GET', 'POST'])
+def add_me():
     current_device_IP = request.remote_addr
     # Find device with this ip in the database
     device = Device.query.filter_by(ip_address=current_device_IP).first()
     if device:
         return jsonify({ 
-            'msg': 'You already exist in the database',
+            'msg': 'You are already exist in the database',
             'device': device.to_dict()
         }), 303
 
     # Add new device to database
-    newDevice = Device(ip_address=current_device_IP, event=event)
+    newDevice = Device(ip_address=current_device_IP)
     db.session.add(newDevice)
     db.session.commit()
 
-    return jsonify({ 'added': newDevice.to_dict()}), 201
+    return jsonify({ 'added': newDevice.to_dict() }), 201
 
 @app.route('/delete/<address>', methods=['GET', 'POST'])
 def delete_device(address):
@@ -110,7 +116,6 @@ def delete_device(address):
         return jsonify({ 'deleted': device.to_dict() }), 201
 
     return jsonify({ 'msg': 'Not exist in the database', 'address': address }), 303
-
 
 
 if __name__ == '__main__':
